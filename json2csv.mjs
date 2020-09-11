b73aa9d9dbde912435215148540aa80e26ec5018
Raul Lopes <raulfpl@gmail.com>
2020-09-11 19:03:55 -0300

    implementa versão simples do conversor

--- json2csv.mjs
+++ json2csv.mjs
@@ -1,3 +1,16 @@
-export default json => {
-  return ''
+// can throw SyntaxError
+const json2obj = json => {
+  const parsed = JSON.parse(json)
+  if (typeof parsed !== 'object' || Array.isArray(parsed)) { // TODO: implementar
+    throw new Error('Não implementado')
+  }
+  return Object.entries(parsed)
 }
+
+const array2csv = line => line.join()
+
+const matrix2csv = matrix => matrix.map(array2csv).join('\n')
+
+const json2csv = json => matrix2csv(json2obj(json))
+
+export default json2csv
