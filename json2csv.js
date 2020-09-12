ecad5c24e283b50653d2a821d2779293da5aa371
Raul Lopes <raulfpl@gmail.com>
2020-09-12 18:39:09 -0300

    adiciona suporte a JSON array

--- json2csv.js
+++ json2csv.js
@@ -1,10 +1,44 @@
-// can throw SyntaxError
+/** @throws SyntaxError */
 const json2matrix = json => {
   const parsed = JSON.parse(json)
-  if (typeof parsed !== 'object' || Array.isArray(parsed)) { // TODO: implementar
+  if (typeof parsed !== 'object') { // TODO: implementar
     throw new Error('Não implementado')
   }
-  return Object.entries(parsed)
+  const mat = []
+  if (Array.isArray(parsed)) {
+    for (const obj of parsed) {
+      addObjToMatrix(obj, mat)
+    }
+  } else {
+    addObjToMatrix(parsed, [])
+  }
+  return mat
+}
+
+const addObjToMatrix = (obj, mtx) => {
+  if (mtx.length === 0) {
+    mtx.push([])
+  }
+  const novaLinha = []
+  const cabeçalho = mtx[0]
+  for (const key in obj) {
+    let newIdx = -1
+    cabeçalho.forEach((cell, idx) => {
+      if (cell === key) {
+        newIdx = idx
+      }
+    })
+    if (newIdx === -1) {
+      newIdx = cabeçalho.push(key) - 1
+      for (let i = 1; i < mtx.length; i++) {
+        mtx[i].length++
+      }
+    }
+    novaLinha[newIdx] = obj[key]
+  }
+  novaLinha.length = mtx[0].length
+  mtx.push(novaLinha)
+  return mtx
 }
 
 const array2csv = line => line.map(corrigeCelula).join()
