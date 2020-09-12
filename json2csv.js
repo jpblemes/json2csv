2d58c9f71a267f5e6abce4e5b36c5c31d6cea970
Raul Lopes <raulfpl@gmail.com>
2020-09-12 18:52:11 -0300

    comentários

--- json2csv.js
+++ json2csv.js
@@ -15,30 +15,39 @@ const json2matrix = json => {
   return mat
 }
 
-const addObjToMatrix = (obj, mtx) => {
-  if (mtx.length === 0) {
-    mtx.push([])
+/**
+ * Adiciona um objeto a uma tabela, sendo a primeira linha o cabeçalho contendo
+ * as chaves de cada valor dos objetos e cada linha seguinte representa um objeto
+ * adicionado
+ * @param obj - objeto
+ * @param mat - matriz representando a tabela */
+const addObjToMatrix = (obj, mat) => {
+  // verifica se já existe cabeçalho na tabela e adiciona, caso contrario
+  if (mat.length === 0) {
+    mat.push([])
   }
+  const cabeçalho = mat[0]
   const novaLinha = []
-  const cabeçalho = mtx[0]
   for (const key in obj) {
+    /** número da coluna  */
     let newIdx = -1
     cabeçalho.forEach((cell, idx) => {
       if (cell === key) {
         newIdx = idx
       }
     })
+    // caso a chave não esteja presente no cabeçado da tabela até então, a adiciona
     if (newIdx === -1) {
       newIdx = cabeçalho.push(key) - 1
-      for (let i = 1; i < mtx.length; i++) {
-        mtx[i].length++
+      for (let i = 1; i < mat.length; i++) {
+        mat[i].length++
       }
     }
     novaLinha[newIdx] = obj[key]
   }
-  novaLinha.length = mtx[0].length
-  mtx.push(novaLinha)
-  return mtx
+  novaLinha.length = mat[0].length
+  mat.push(novaLinha)
+  return mat
 }
 
 const array2csv = line => line.map(corrigeCelula).join()
