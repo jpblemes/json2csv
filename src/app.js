29a648fac6bc33d2eaeba7c9e6b58b0418dbeb14
Raul Lopes <raulfpl@gmail.com>
2020-09-14 13:13:33 -0300

    transforma o app em PWA

--- src/app.js
+++ src/app.js
@@ -116,3 +116,15 @@ const limpaTabela = () => {
   tabela.classList.add('hidden')
   h1Tabela.classList.add('hidden')
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  })
+}
