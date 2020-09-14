42d370ce40a2f05247d3b55a76ef8b036704400b
Raul Lopes <raulfpl@gmail.com>
2020-09-14 11:50:46 -0300

    adiciona script de build do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: ['index.html', 'app.js']
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
